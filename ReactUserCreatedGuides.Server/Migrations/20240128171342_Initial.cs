5709e5374b9904f822bb6114fcf7d2e50a1644e6
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2024-01-28 09:54:05 -0800

    Created Initial Migration

---
+++ ReactUserCreatedGuides.Server/Migrations/20240128171342_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReactUserCreatedGuides.Server.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Guides",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProgrammingLanguage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BriefSummary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DetailedGuide = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guides", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guides");
+        }
+    }
+}
